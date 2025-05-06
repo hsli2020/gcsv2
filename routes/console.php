68cf16663947a7e0f47d6211cf8196d996e4e640
Hanson Li <lihsca@gmail.com>
2025-05-05 22:33:23 -0400

    DemoCron command

--- routes/console.php
+++ routes/console.php
@@ -6,3 +6,6 @@ use Illuminate\Support\Facades\Artisan;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+#php artisan cron:demo
+#Schedule::command('cron:demo')->everyTenSeconds();
