201b5d48349d2b52633efe53f594469db4c1b4fd
Hanson Li <lihsca@gmail.com>
2025-05-07 13:15:19 -0400

    ExportController

---
+++ app/Http/Controllers/ExportController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ExportController extends Controller
+{
+    public function index()
+    {
+    }
+
+    public function daily()
+    {
+    }
+
+    public function combiner()
+    {
+    }
+}
