07ea36c58d4fa123f83c194c80d1f28de4e0346b
Hanson Li <lihsca@gmail.com>
2025-05-07 22:30:32 -0400

    views - report-daily and report-monthly

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -10,12 +10,12 @@ class ReportController extends Controller
 {
     public function daily()
     {
-        // return view('report-daily')
+        return view('report-daily')
     }
 
     public function monthly()
     {
-        // return view('report-monthly')
+        return view('report-monthly')
     }
 
     public function budget($id = 1)
